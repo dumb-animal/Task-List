ff5745cca250a37053fe7379c8fcf3799f54a176
dumb-animal <dumb_animal@icloud.com>
2020-11-19 02:21:39 +0300

    Update App.cpp

--- classes/App.cpp
+++ classes/App.cpp
@@ -11,24 +11,26 @@ public:
 private:
     TaskList list;
 
-    enum options
+    enum main_menu
     {
         Exit,
         Add_Tasks,
         Show_Tasks,
         Change_Task,
         Delete_Task,
+        settings,
     };
 
     void ShowMenu()
     {
         clear_console();
         cout << "Menu:" << endl;
-        cout << options::Add_Tasks << " Add tasks" << endl;
-        cout << options::Show_Tasks << " Show tasks" << endl;
-        cout << options::Change_Task << " Change task" << endl;
-        cout << options::Delete_Task << " Delete task" << endl;
-        cout << options::Exit << " exit" << endl;
+        cout << main_menu::Add_Tasks << " Add tasks" << endl;
+        cout << main_menu::Show_Tasks << " Show tasks" << endl;
+        cout << main_menu::Change_Task << " Change task" << endl;
+        cout << main_menu::Delete_Task << " Delete task" << endl;
+        cout << main_menu::settings << " Settings" << endl;
+        cout << main_menu::Exit << " exit" << endl;
     }
 
     void loop()
@@ -40,19 +42,19 @@ private:
             int option = ReadNumber();
             switch (option)
             {
-            case options::Add_Tasks:
+            case main_menu::Add_Tasks:
                 list.AddTasks();
                 break;
-            case options::Show_Tasks:
+            case main_menu::Show_Tasks:
                 list.ShowTasks();
                 break;
-            case options::Change_Task:
+            case main_menu::Change_Task:
                 list.ChangeTask();
                 break;
-            case options::Delete_Task:
+            case main_menu::Delete_Task:
                 list.DeleteTask();
                 break;
-            case options::Exit:
+            case main_menu::Exit:
                 isExit = true;
                 break;
             }
